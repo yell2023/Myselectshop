5a4a07e9a76fe0424e05aa2d4ca1abace8ca0f7c
Yang ye lin <oky07031217@gmail.com>
2024-11-05 21:55:21 +0900

    관심 상품에 폴더 추가 및 폴더 전체 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -44,6 +44,16 @@ public class ProductController {
         return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
 
+    // 상품에 폴더 추가
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
+
     // 관리자 조회
     @GetMapping("/admin/products")
     public List<ProductResponseDto> getAllProducts() {
