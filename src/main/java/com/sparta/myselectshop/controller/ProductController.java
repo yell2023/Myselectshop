159557183fe7503084af8fd5325e4ad83e11fbc2
Yang ye lin <oky07031217@gmail.com>
2024-11-05 12:15:42 +0900

    관심상품 조회 API

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -14,15 +16,21 @@ public class ProductController {
 
     private final ProductService productService;
 
-    // 관심상품 등록
+    // 관심 상품 등록
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto){
         return productService.createProduct(requestDto);
     }
 
-    // 관심상품 희망 최저가 등록
+    // 관심 상품 희망 최저가 등록
     @PutMapping("/products/{id}")
     public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         return productService.updateProduct(id, requestDto);
+    }
+
+    // 관심 상품 조회
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        return productService.getProducts();
     }
 }
